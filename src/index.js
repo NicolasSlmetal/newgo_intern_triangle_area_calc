1ee7b139909127b5717c1b931b3f5acc6863f140
Nicolas <nlimadasilvabrito@gmail.com>
2024-07-30 17:04:00 -0300

    feat: implement reading of the sides and angle of triangle and calculate
    This commit will implement the reading of sides and angle of a new triangle and after, calculate the area using these parameters. This follows the first reading of the base and height triangle

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import readline from 'readline'
 var base = 0
 var height = 0
 
+var sideA = 0
+var sideB = 0
+var angle = 0
+
 var reader = readline.createInterface(process.stdin, process.stdout)
 
 
@@ -20,9 +24,36 @@ function askHeight(){
         height = Number.parseFloat(answer)
         var triangleBaseHeight = new TriangleBaseHeight(base, height)
         console.log("A área do triângulo é:", triangleBaseHeight.calculateArea())
+        console.log("CÁLCULO DA ÁREA DO TRIÂNGULO COM (a * b * sen(O))/2")
+        askSideA()
+    })
+}
+
+function askSideA(){
+    reader.question("Digite o comprimento do primeiro lado: ", function(answer){
+        sideA = Number.parseFloat(answer)
+        askSideB()
+    })
+}
+
+function askSideB(){
+    reader.question("Digite o comprimento do segundo lado: ", function(answer){
+        sideB = Number.parseFloat(answer)
+        askAngle()
+    })
+}
+
+function askAngle(){
+    reader.question("Digite o ângulo entre os lados em graus:", function(answer){
+        angle = Number.parseFloat(answer)
+        var triangleSideAngle = new TriangleSideAngle(sideA, sideB, angle)
+        console.log("A área do triângulo é: ", triangleSideAngle.calculateArea())
         reader.close()
     })
 }
 
 console.log("CÁLCULO DA ÁREA DO TRIÂNGULO COM (B * H)/2")
 askBase()
+
+
+
