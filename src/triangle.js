88d27d3bd48b93576a20a07aa858a59931795f9b
Nicolas <nlimadasilvabrito@gmail.com>
2024-07-30 10:49:20 -0300

    feat: add TriangleSideAngle class
    This commit will introduce the TriangleSideAngle class, which will implement the method for calculating the area with the formula (a * b * sin(O))/2, where a and b are sides of the triangle and O is the angle between them. The method will be implemented in a future commit

--- src/triangle.js
+++ src/triangle.js
@@ -14,3 +14,11 @@ class TriangleBaseHeight extends Triangle{
         return (base * height)/2
     }
 }
+
+class TriangleSideAngle extends Triangle{
+    constructor(sideA, sideB, angle){
+        this.sideA = sideA
+        this.sideB = sideB
+        this.angle = angle
+    }
+}
