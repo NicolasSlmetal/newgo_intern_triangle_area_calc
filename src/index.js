43891e2e1da1130d7992c9c8673e83f672d47728
Nicolas <nlimadasilvabrito@gmail.com>
2024-07-30 16:18:24 -0300

    feat: add readline to read values of base and height and calculate the area
    This commit will implement the import of readline to read base and height from user and calculate the area with these two parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import { Triangle, TriangleBaseHeight, TriangleSideAngle } from "./triangle.js";
+import readline from 'readline'
+
+var base = 0
+var height = 0
+
+var reader = readline.createInterface(process.stdin, process.stdout)
 
 
+
+function askBase(){
+    reader.question("Digite o valor da base do triângulo: ", function(answer){
+        base = Number.parseFloat(answer)
+        askHeight()
+    })
+}
+
+function askHeight(){
+    reader.question("Digite o valor da altura do triângulo: ", function(answer){
+        height = Number.parseFloat(answer)
+        var triangleBaseHeight = new TriangleBaseHeight(base, height)
+        console.log("A área do triângulo é:", triangleBaseHeight.calculateArea())
+        reader.close()
+    })
+}
+
+console.log("CÁLCULO DA ÁREA DO TRIÂNGULO COM (B * H)/2")
+askBase()
